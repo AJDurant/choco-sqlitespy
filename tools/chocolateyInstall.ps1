f68aa767d5dd0c6e4f803a97c93926d93fc0c9f7
Andy Durant <adurant@rapitasystems.com>
2023-10-13 14:03:38 +0100

    upgrade to v1.9.19

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -11,7 +11,7 @@ Else {
 
 $packageArgs = @{
     PackageName    = $env:ChocolateyPackageName
-    FileFullPath   = "$toolsDir/SQLiteSpy_v1.9.17.zip"
+    FileFullPath   = "$toolsDir/SQLiteSpy_v1.9.19.zip"
     Destination    = $toolsDir
     SpecificFolder = $folder
 }
